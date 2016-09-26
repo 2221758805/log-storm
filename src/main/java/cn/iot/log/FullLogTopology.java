5de938b0fbadc7b0be258b2cd1c0397ffc13e50f
macrowh <macrowh@gmail.com>
2016-09-26 15:19:29 +0800

    提交相关依赖

--- src/main/java/cn/iot/log/FullLogTopology.java
+++ src/main/java/cn/iot/log/FullLogTopology.java
@@ -1,13 +1,17 @@
 package cn.iot.log;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
 import org.apache.storm.elasticsearch.bolt.EsIndexBolt;
 import org.apache.storm.elasticsearch.common.EsConfig;
 import org.apache.storm.elasticsearch.common.EsTupleMapper;
+import org.apache.storm.generated.StormTopology;
 import org.apache.storm.kafka.KafkaSpout;
 import org.apache.storm.kafka.SpoutConfig;
 import org.apache.storm.kafka.StringScheme;
@@ -30,14 +34,58 @@ public class FullLogTopology {
 		TopologyBuilder builder = new TopologyBuilder();
 		builder.setSpout("spout", kafkaSpout, 4);
 		builder.setBolt("fulllog", new FullLogBolt(), 10).shuffleGrouping("spout");
-//		builder.setBolt("printlog", new PrintBolt(), 4).shuffleGrouping("fulllog");
+		builder.setBolt("printlog", new PrintBolt(), 4).shuffleGrouping("fulllog");
 		builder.setBolt("esbolt", esIndexBolt, 4).shuffleGrouping("fulllog");
+		StormTopology topology = builder.createTopology();
+		startClusterTopology(topology);
+//		startLocalTopology(topology);
+	}
 
-		Config conf = new Config();
+	private static void startLocalTopology(StormTopology topology) throws Exception {
+		logger.info("Begin to start local topology.......");
+		Config config = new Config();
 		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology("storm-log", conf, builder.createTopology());
-		// Thread.sleep(6000000);
-		// cluster.shutdown();
+		cluster.submitTopology("storm-log", config, topology);
+		Thread.sleep(60000);
+		cluster.shutdown();
+		logger.info("Finish start local topology");
+	}
+
+	private static void startClusterTopology(StormTopology topology) throws Exception {
+		logger.info("Begin to start cluster topology.......");
+		Config config = new Config();
+		config.put(Config.NIMBUS_SEEDS, parseNimbus("192.168.156.60"));
+		config.put(Config.NIMBUS_THRIFT_PORT, Integer.parseInt("6627"));
+		config.put(Config.STORM_ZOOKEEPER_PORT, parseZkPort("192.168.156.58:2181,192.168.156.60:2181"));
+		config.put(Config.STORM_ZOOKEEPER_SERVERS, parseZkHosts("192.168.156.58:2181,192.168.156.60:2181"));
+		config.setNumWorkers(10);
+		config.setMaxSpoutPending(5000);
+		StormSubmitter.submitTopology("storm-log", config, topology);
+		logger.info("Finish start cluster topology");
+	}
+
+	private static List<String> parseNimbus(String nimbus){
+		String[] hostsAndPorts = nimbus.split(",");
+		List<String> hosts = new ArrayList<String>(hostsAndPorts.length);
+		for (int i = 0; i < hostsAndPorts.length; i++) {
+			hosts.add(i, hostsAndPorts[i].split(":")[0]);
+		}
+		return hosts;
+	}
+	
+	private static List<String> parseZkHosts(String zkNodes) {
+		String[] hostsAndPorts = zkNodes.split(",");
+		List<String> hosts = new ArrayList<String>(hostsAndPorts.length);
+		for (int i = 0; i < hostsAndPorts.length; i++) {
+			hosts.add(i, hostsAndPorts[i].split(":")[0]);
+		}
+		return hosts;
+	}
+
+	private static int parseZkPort(String zkNodes) {
+		String[] hostsAndPorts = zkNodes.split(",");
+		int port = Integer.parseInt(hostsAndPorts[0].split(":")[1]);
+		return port;
 	}
 
 	private static KafkaSpout createKafkaSpout() {
